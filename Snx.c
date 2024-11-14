0f5b28e67a4fd6142d4dbc648450412c4c13ac1a
AnuragKumar726007 <anuragkumar726007@gmail.com>
2024-11-14 09:36:13 +0000

    Create Snx .c

---
+++ Snx.c
@@ -0,0 +1,19 @@
+int main()
+{
+    // Print Name 
+    printf("Name : Anurag kumar \n");
+
+    // Print Date of Birth 
+    printf("DOB : January 01, 2005 \n");
+
+    // Print Mobile Number
+    printf("Mobile : 7260072903 \n");
+
+    //Indicate successful execution 
+    return 0;
+
+
+}
+
+
+
